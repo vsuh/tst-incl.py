b76a6d5e7a04bb98bdeefeeb9b265eff04e66c17
cyx <vsuh@mail.ru>
2025-06-04 12:06:35 +0300

    python-dotenv lib installed

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
-
+from dotenv import load_dotenv
 print(f"""          *** TEST SCRIPT ***
-      ENV "TESTING12" = {os.getenv("TESTING12", "fuck")}
+      ENV "TESTING12" = {os.getenv("TESTING12", "not set")}
 
 """)
